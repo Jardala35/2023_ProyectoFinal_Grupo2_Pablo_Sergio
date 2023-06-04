c43005a6ba055defe37fd69775cdb6a7494ca1f0
Pablo Navarro <pablonavarroduro@gmail.com>
2023-06-04 13:44:28 +0200

    peliculas.html

---
+++ pelicula.js
@@ -0,0 +1,297 @@
+
+
+var fragmentosHTML = [
+    //1
+    `<article class="art">
+    <div class="cont1">
+        <h1 class="titulo">Cadena perpetua</h1>
+        <img class="imgpel" src="imgs/Cadena_perpetua.jpg" />
+        <p class="texto">
+            Sinopsis:
+            Andy Dufresne es encarcelado por matar a su esposa y al amante de esta. Tras una dura adaptación,
+            intenta mejorar las condiciones de la prisión y dar esperanza a sus compañeros.
+        </p>
+        <h6 class="texto2">Año: 1994</h6>
+        <p class="texto2">Director: Frank Darabont</p>
+        <p class="texto2">Valoracion: 9,3/10</p>
+    </div>
+    <div class="cont2">
+        <h3>Trailer</h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/r5X-hFf6Bwo"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+    </div>
+  </article>`,
+    //2
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Alien, El octavo pasajero</h1>
+      <img class="imgpel" src="imgs/Alien.jpg" />
+      <p class="texto">
+          Sinopsis:
+          La tripulación de una nave espacial comercial se encuentra con una forma de vida mortal tras investigar una transmisión desconocida.
+      </p>
+      <h6 class="texto2">Año: 1979</h6>
+      <p class="texto2">Director: Ridley Scott</p>
+      <p class="texto2">Valoracion: 8,5/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Eu9ZFTXXEiw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //3
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Vengadores, Infinity war</h1>
+      <img class="imgpel" src="imgs/avengers-infinity-war.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Los Vengadores y sus aliados estarán dispuestos a sacrificarlo todo para derrotar al poderoso Thanos antes de que su devastación y ruina ponga fin al universo.
+      </p>
+      <h6 class="texto2">Año: 2018</h6>
+      <p class="texto2">Director: Anthony Russo y Joe Russo</p>
+      <p class="texto2">Valoracion: 8,4/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/-f5PwE_Q8Fs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //4
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">El silencio de los corderos</h1>
+      <img class="imgpel" src="imgs/El_silencio_de_los_corderos.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Una joven cadete del FBI busca la ayuda de un asesino caníbal y manipulador encarcelado con el fin de atrapar a otro asesino en serie, un loco que despelleja a sus víctimas.
+      </p>
+      <h6 class="texto2">Año: 1991</h6>
+      <p class="texto2">Director: Jonathan Demme</p>
+      <p class="texto2">Valoracion: 8,6/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/mDKn4VL8o10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //5
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Gladiator</h1>
+      <img class="imgpel" src="imgs/Gladiator.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Un exgeneral romano se propone vengarse del emperador corrupto que asesinó a su familia y lo envió a la esclavitud.
+      </p>
+      <h6 class="texto2">Año: 2000</h6>
+      <p class="texto2">Director: Ridley Scott</p>
+      <p class="texto2">Valoracion: 8,5/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/W3xjHIdaGww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //6
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Interstellar</h1>
+      <img class="imgpel" src="imgs/Interstellar.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Un equipo de exploradores viaja a través de un agujero de gusano en el espacio en un intento de garantizar la supervivencia de la humanidad.
+      </p>
+      <h6 class="texto2">Año: 2014</h6>
+      <p class="texto2">Director: Christopher Nolan</p>
+      <p class="texto2">Valoracion: 8,7/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/UoSSbmD9vqc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //7
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">El señor de los anillos, El retorno del rey</h1>
+      <img class="imgpel" src="imgs/Lord_of_rings.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Gandalf y Aragorn lideran el mundo de los hombres contra la armada de Sauron para distraer su atención de Frodo y Sam, quienes se aproximan al Monte del Destino con el Anillo Único.
+      </p>
+      <h6 class="texto2">Año: 2003</h6>
+      <p class="texto2">Director: Peter Jackson</p>
+      <p class="texto2">Valoracion: 9,0/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/P4PEN0BNDcw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //8
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Matrix</h1>
+      <img class="imgpel" src="imgs/Matrix.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Cuando una bella desconocida lleva al hacker Neo a un inframundo prohibido, descubre la impactante verdad: la vida que conoce es un elaborado engaño de una ciberinteligencia malvada.
+      </p>
+      <h6 class="texto2">Año: 1999</h6>
+      <p class="texto2">Director: Lana Wachowski y Lilly Wachowski</p>
+      <p class="texto2">Valoracion: 8,7/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/vN_Hx_It3r0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //9
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Origen</h1>
+      <img class="imgpel" src="imgs/origen.jpg" />
+      <p class="texto">
+          Sinopsis:
+          A un ladrón que roba secretos corporativos a través del uso de la tecnología de compartir sueños, se le da la tarea de implantar una idea en la mente de un jefe de una gran empresa.
+      </p>
+      <h6 class="texto2">Año: 2010</h6>
+      <p class="texto2">Director: Christopher Nolan</p>
+      <p class="texto2">Valoracion: 8,8/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/RV9L7ui9Cn8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //10
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Pulp fiction</h1>
+      <img class="imgpel" src="imgs/Pulp_fiction.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Las vidas de dos mafiosos, un boxeador, la esposa de un gánster y un par de bandidos se entrelazan en cuatro historias de violencia y redención.
+      </p>
+      <h6 class="texto2">Año: 1994</h6>
+      <p class="texto2">Director: Quentin Tarantino</p>
+      <p class="texto2">Valoracion: 8,9/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/auCgsj0MV-M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //11
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">El resplandor</h1>
+      <img class="imgpel" src="imgs/resplandor.jpg />
+      <p class="texto">
+          Sinopsis:
+          Una familia se dirige a un hotel aislado para pasar el invierno. Allí, una presencia espiritual maligna violenta al padre, mientras el hijo, psíquico, tiene horripilantes visiones del pasado y del futuro.
+      </p>
+      <h6 class="texto2">Año: 1980</h6>
+      <p class="texto2">Director: Stanley Kubrick</p>
+      <p class="texto2">Valoracion: 8,4/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/IiSjPcRWjYA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //12
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Terminator 2, el juicio final</h1>
+      <img class="imgpel" src="imgs/Terminator_2_El_juicio_final.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Un cyborg, idéntico al que fracasó en su intento de matar a Sarah Connor, debe proteger ahora a su hijo adolescente John de un cyborg más avanzado y poderoso.
+      </p>
+      <h6 class="texto2">Año: 1991</h6>
+      <p class="texto2">Director: James Cameron</p>
+      <p class="texto2">Valoracion: 8,6/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/mVbYhayCdm0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //13
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">El caballero oscuro</h1>
+      <img class="imgpel" src="imgs/the_dark_knight.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Cuando la amenaza conocida como el Joker causa estragos y el caos en Gotham City, Batman debe aceptar una de las mayores pruebas psicológicas y físicas para luchar contra la injusticia.
+      </p>
+      <h6 class="texto2">Año: 2008</h6>
+      <p class="texto2">Director: Christopher Nolan</p>
+      <p class="texto2">Valoracion: 9,0/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Qs-NylETt1E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //14
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Malditos bastardos</h1>
+      <img class="imgpel" src="imgs/inglourious_basterds.jpg" />
+      <p class="texto">
+          Sinopsis:
+          En la Francia ocupada por los nazis durante la Segunda Guerra Mundial, un plan para asesinar a los líderes nazis por parte de un grupo de soldados judíos estadounidenses coincide con los planes vengativos de la dueña de un cine.
+      </p>
+      <h6 class="texto2">Año: 2009</h6>
+      <p class="texto2">Director: Quentin Tarantino</p>
+      <p class="texto2">Valoracion: 8,3/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/2jcTRi8D80k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    //15
+    `<article class="art">
+  <div class="cont1">
+      <h1 class="titulo">Old boy</h1>
+      <img class="imgpel" src="imgs/Old_boy.jpg" />
+      <p class="texto">
+          Sinopsis:
+          Tras ser secuestrado y aprisionado durante 15 años, Oh Dae-Su es liberado y tiene 5 días para encontrar a su captor.
+      </p>
+      <h6 class="texto2">Año: 2003</h6>
+      <p class="texto2">Director: Park Chan-wook</p>
+      <p class="texto2">Valoracion: 8,4/10</p>
+  </div>
+  <div class="cont2">
+      <h3>Trailer</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/-W8cPij0HgA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</article>`,
+    
+];
+
+
+function ejecutarCodigoHTML(indice) {
+    
+    if (indice >= 0 && indice < fragmentosHTML.length) {
+        var contenedor = document.getElementById("peli");
+        contenedor.innerHTML = fragmentosHTML[indice];
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    
+    var parametros = new URLSearchParams(window.location.search);
+    var codigo = parametros.get("codigo");
+
+    
+    ejecutarCodigoHTML(codigo);
+});
